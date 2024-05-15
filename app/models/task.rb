8bf5c4ba33a65134671a0de1bcea7c242e13b8ba
Anton921 <62765431+Anton921@users.noreply.github.com>
2024-05-15 14:43:19 +0300

    update_tasks_status_enum

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,7 @@ class Task < ApplicationRecord
   belongs_to :project
   validates_presence_of :name, :description
 
-  enum :status, %i[new_task in_progress completed], validate: true
+  enum status: { new_task: 0, in_progress: 1, completed: 2 }
 
   scope :by_status, lambda { |status|
                       status.blank? ? where({}) : where(status:)
